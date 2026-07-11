e2681398056d8b28852faad8d89edf9f34ce95bb
Selinam <selinam.mortey@yale.edu>
2026-07-11 14:56:05 -0700

    Updated filter mechnism to a Windowed-Sinc FIR using a Blackman window

--- device/phase1_imu/phase1_imu.ino
+++ device/phase1_imu/phase1_imu.ino
@@ -2,38 +2,75 @@
 
 unsigned long lastSample = 0;
 const unsigned long interval = 20;
-const int WINDOW = 5;
+const int WINDOW = 31;
+const float pi = 3.14159f;
+const float cutoff_frequency = 0.14f;
+const int M = WINDOW - 1;
 
-struct MovingAverage
+float kernel[WINDOW];
+
+struct FIR
 {
-  float sum;
-  float buffer[WINDOW];
   int index;
+  float buffer[WINDOW];
 };
 
-void init_MovingAverage(MovingAverage &ma) {
-  for (int i = 0; i < WINDOW; i++){
-    ma.buffer[i] = 0;
+void init_FIR(FIR &f);
+float update_FIR(FIR &f, float sample);
+
+void init_kernel () {
+  float sum = 0;
+  for (int i = 0; i < WINDOW; i++) {
+    int offset = i - M / 2;
+    if (offset == 0) {
+      kernel[i] = 2*pi*cutoff_frequency;
+    }
+    else {
+      kernel[i] = sin(2*pi*cutoff_frequency * offset) / (offset);
+    }
+    kernel[i] = kernel[i] * (0.42f - 0.5f * cos((2 * pi * i) / M) + 0.08f * cos((4 * pi * i) / M));
+
   }
-  ma.sum = 0.0f;
-  ma.index = 0;
+
+  for (int i = 0; i < WINDOW; i++) {
+    sum += kernel[i];
+  }
+
+  for (int i = 0; i < WINDOW; i++) {
+    kernel[i] = kernel[i] / sum;
+  }
+
 }
 
-float update_MovingAverage (MovingAverage &ma, float sample) {
-  ma.sum -= ma.buffer[ma.index];
-  ma.buffer[ma.index] = sample;
-  ma.sum += ma.buffer[ma.index];
-  ma.index = (ma.index + 1) % WINDOW;
-  return ma.sum / WINDOW;
+
+
+void init_FIR (FIR &f) {
+  for (int i = 0; i <WINDOW; i++){
+    f.buffer[i] = 0;
+  }
+  f.index = 0;
 }
 
-MovingAverage maX;
-MovingAverage maY;
-MovingAverage maZ;
+float update_FIR (FIR &f, float sample) {
+  f.buffer[f.index] = sample;
+  f.index = (f.index + 1) % WINDOW;
+
+  float acc = 0.0f;
+  int j = f.index;
+
+  for (int i  = WINDOW - 1; i >= 0; i--) {
+    acc += (kernel[i] * f.buffer[j]);
+    j = (j + 1) % WINDOW;
+  }
+  return acc;
+}
+
+FIR fx;
+FIR fy;
+FIR fz;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
   Serial.println("Started");
 
   if (!IMU.begin()) {
@@ -48,10 +85,11 @@ void setup() {
   Serial.println("Acceleration in g's");
   Serial.println("X\tY\tZ");
 
-
-  init_MovingAverage(maX);
-  init_MovingAverage(maY);
-  init_MovingAverage(maZ);
+  init_kernel();
+  
+  init_FIR(fx);
+  init_FIR(fy);
+  init_FIR(fz);
 }
 
 void loop() {
@@ -64,9 +102,9 @@ void loop() {
     if (IMU.accelerationAvailable()) {
       IMU.readAcceleration(x, y, z);
 
-      float filter_x = update_MovingAverage(maX, x);
-      float filter_y = update_MovingAverage(maY, y);
-      float filter_z = update_MovingAverage(maZ, z);
+      float filter_x = update_FIR(fx, x);
+      float filter_y = update_FIR(fy, y);
+      float filter_z = update_FIR(fz, z);
 
       Serial.print(filter_x);
       Serial.print('\t');
